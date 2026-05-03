06b2939e139e4dc8ff878c635104edd80a0518a7
khmo112233789 <khmo8192920@gmail.com>
2026-05-03 22:05:15 +0300

    The user service has been migrated, and the enums, repository, and createCommandHandler have also been added.

---
+++ src/Services/User/API/Program.cs
@@ -0,0 +1,42 @@
+
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            builder.Services.AddDbContext<UserAppDbContext>(optionBuilder =>
+            {
+                optionBuilder.UseSqlServer(builder.Configuration.GetConnectionString("constr"));
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ src/Services/User/Application/Dtos/UpdateUserProfileRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public sealed record UpdateUserProfileRequest(
+        string FirstName,
+        string LastName,
+        string? Bio,
+        string? AvatarUrl,
+        string? PhoneNumber);
+}
