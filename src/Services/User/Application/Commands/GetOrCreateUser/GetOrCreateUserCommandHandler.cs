44d4705485326613452a2b5a4198eadb8fb6ab62
khmo112233789 <khmo8192920@gmail.com>
2026-05-04 15:56:42 +0300

    The user service has been migrated, and the enums, repository, and createCommandHandler have also been added.

--- src/Services/User/Application/Commands/GetOrCreateUser/GetOrCreateUserCommandHandler.cs
+++ src/Services/User/Application/Commands/GetOrCreateUser/GetOrCreateUserCommandHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Application.Dtos;
+using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,13 @@ using System.Threading.Tasks;
 
 namespace Application.Commands.GetOrCreateUser
 {
-    internal class GetOrCreateUserCommandHandler
+    public class GetOrCreateUserCommandHandler(
+        IUserRepository repository,
+        ILogger<GetOrCreateUserCommandHandler> logger) : IRequestHandler<GetOrCreateUserCommand, UserProfileDto>
     {
+        public Task<UserProfileDto> Handle(GetOrCreateUserCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
