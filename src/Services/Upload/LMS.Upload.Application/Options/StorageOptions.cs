30635300ea8aa8118ea56f7bd5dbefb64c4671da
zeezo679 <ziadabdalla105@gmail.com>
2026-05-08 23:51:38 +0300

    update: StorageOptions and appsettings.json

--- src/Services/Upload/LMS.Upload.Application/Options/StorageOptions.cs
+++ src/Services/Upload/LMS.Upload.Application/Options/StorageOptions.cs
@@ -3,4 +3,5 @@ namespace LMS.Upload.Application.Options;
 public class StorageOptions
 {
     public string LocalStorageRootPath { get; set; } = null!;
+    public string BaseUrl { get; set; } = null!;
 }
