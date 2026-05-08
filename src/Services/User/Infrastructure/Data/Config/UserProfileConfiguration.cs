9bed9804c3fdb11631987b4839390baa58c37636
khmo112233789 <khmo8192920@gmail.com>
2026-05-08 16:21:39 +0300

    The infrastructure folder issue has been resolved and the paths have been modified.

---
+++ src/Services/User/Infrastructure/Data/Config/UserProfileConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Config
+{
+    public class UserProfileConfiguration : IEntityTypeConfiguration<UserProfile>
+    {
+        public void Configure(EntityTypeBuilder<UserProfile> builder)
+        {
+            builder.ToTable("UserProfiles");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(u => u.Id)
+            .ValueGeneratedNever();
+
+            // Each AuthUserId maps to exactly one profile
+            builder.HasIndex(x => x.AuthUserId)
+                .IsUnique();
+
+            builder.HasIndex(x => x.Email)
+            .IsUnique();
+
+            builder.Property(x => x.Role)
+            .HasConversion<string>()
+            .HasMaxLength(20)
+            .IsRequired();
+
+            builder.Property(x => x.Status)
+            .HasConversion<string>()
+            .HasMaxLength(20)
+            .IsRequired();
+
+            builder.Property(u => u.LastModifiedAt);
+
+            builder.Property(u => u.LastModifiedBy)
+                .HasMaxLength(256);
+
+
+            builder.Ignore(u => u.FullName);
+        }
+    }
+}
